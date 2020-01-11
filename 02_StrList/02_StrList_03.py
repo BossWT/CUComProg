1bafc32fc99694fd73c3b1040d3299bad70e678b
bosswt <boss.wt13@gmail.com>
2020-01-12 03:27:14 +0700

    Formatting and Simplify

--- 02_StrList/02_StrList_03.py
+++ 02_StrList/02_StrList_03.py
@@ -1,22 +1,8 @@
 str = input()
-month = ['January','February','March','April','May','June','July','August','September','October','November','December']
-ch = ''
-day = ''
-mon = ''
-i=0
-for s in str:
-    if s!='/':
-        ch+=s
-    else:
-        if i==0:
-            day=ch
-            ch=''
-            i+=1
-        elif i==1:
-            mon=ch
-            ch=''
-            i+=1
-year = str[-4:]
-print(month[int(mon)-1],end=" ")
-print(day,end=", ")
-print(year)
+month = ['x', 'January', 'February', 'March', 'April', 'May', 'June',
+         'July', 'August', 'September', 'October', 'November', 'December']
+date = str.split('/')
+day = date[0]
+mon = date[1]
+year = date[2]
+print(month[int(mon)], day + ',', year, sep=' ')
