50a3fbf1c23dc16ea85ce4998757740e800b5507
bosswt <boss.wt13@gmail.com>
2020-01-12 03:27:50 +0700

    Formatting and Simplify

--- 02_StrList/02_StrList_05.py
+++ 02_StrList/02_StrList_05.py
@@ -1,19 +1,6 @@
 str = input()
-num = ''
-sum=0
-l = len(str)
-i=1
-for s in str:
-    # print(i)
-    if s!=' ':
-        num+=s
-    else:
-        # print(num)
-        if num=='':
-            num=0
-        sum+=int(num)
-        num = ''
-    if i==l:
-        sum+=int(num)
-    i+=1
+n = str.split()
+sum = 0
+for i in range(0, 7):
+    sum += int(n[i])
 print(sum)
