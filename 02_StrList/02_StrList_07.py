722e543af328b2964868d63005b774529886691a
bosswt <boss.wt13@gmail.com>
2020-01-12 03:28:11 +0700

    Formatting

--- 02_StrList/02_StrList_07.py
+++ 02_StrList/02_StrList_07.py
@@ -1,19 +1,13 @@
 n = input()
 a = n[3::7]
 b = n[7::5]
-let = ['-','A','B','C','D','E','F','G','H','I','J']
-sum = int(a)+int(b)+10000
+let = ['-', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
+sum = int(a) + int(b) + 10000
 sum = str(sum)
 code = sum[-4:-1]
-# print(a)
-# print(b)
-# print(sum)
-# print(code)
 sum = 0
 for c in code:
     sum += int(c)
 sum = str(sum)
-index = int(sum[-1])+1
-# print(sum)
-# print(index)
-print(code+""+let[int(index)])
+index = int(sum[-1]) + 1
+print(code + "" + let[int(index)])
