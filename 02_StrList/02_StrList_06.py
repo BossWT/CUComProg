6911cabaedfd5f0663a2e1160ccbf62c21f8ef1f
bosswt <boss.wt13@gmail.com>
2020-01-13 12:08:17 +0700

    Simplify

--- 02_StrList/02_StrList_06.py
+++ 02_StrList/02_StrList_06.py
@@ -1,25 +1,12 @@
 a = input()
 b = input()
-u = []
-v = []
-ans = []
-num = ''
-sum = 0
-for i in range(1, len(a)):
-    if a[i] != ',' and a[i] != ']':
-        num += a[i]
-    else:
-        u.append(float(num))
-        num = ''
-for i in range(1, len(b)):
-    if b[i] != ',' and b[i] != ']':
-        num += b[i]
-    else:
-        v.append(float(num))
-        num = ''
-ans.append(u[0] + v[0])
-ans.append(u[1] + v[1])
-ans.append(u[2] + v[2])
+sa = a[1:-1:]
+sb = b[1:-1:]
+u = sa.split(', ')
+v = sb.split(', ')
+u = list(map(float, u))
+v = list(map(float, v))
+ans = [x + y for x, y in zip(u, v)]
 print(u, end=" + ")
 print(v, end=" = ")
 print(ans)
