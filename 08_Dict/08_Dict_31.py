d2aaa078129a52a3baea79f5fb20c7c182b16799
bosswt <boss.wt13@gmail.com>
2020-04-03 22:51:07 +0700

    Cash 100%

--- 08_Dict/08_Dict_31.py
+++ 08_Dict/08_Dict_31.py
@@ -15,21 +15,48 @@ def take(pocket, money_in):
 
 
 def pay(pocket, amt):
-    d_pay = {}
-    for k, v in pocket.items():
-        while amt >= k and pocket[k] > 0:
-            if k in d_pay:
-                d_pay[k] += 1
+    # Using list to display certain order
+    a = {}
+    new = {}
+    z = []
+    for s in pocket:
+        z.append([s * -1, pocket[s]])
+    z.sort()
+    for e in z:
+        e[0] = int(e[0]) * -1
+    for e in range(len(z)):
+        while z[e][0] <= amt and z[e][1] != 0:
+            if z[e][0] not in a:
+                z[e][1] -= 1
+                a[z[e][0]] = 1
+                amt -= z[e][0]
             else:
-                d_pay[k] = 1
-            amt -= k
-            pocket[k] -= 1
-    if amt == 0:
-        return d_pay
-    else:
-        for k, v in d_pay.items():
-            pocket[k] += v
+                z[e][1] -= 1
+                amt -= z[e][0]
+                a[z[e][0]] += 1
+    if amt != 0:
         return {}
+    for e in range(len(z)):
+        new[z[e][0]] = z[e][1]
+    for e in new:
+        pocket[e] = new[e]
+    return a
+    # Use .items() will display uncertain order of key and value in older python version.
+    # d_pay = {}
+    # for k, v in pocket.items():
+    #     while amt >= k and pocket[k] > 0:
+    #         if k in d_pay:
+    #             d_pay[k] += 1
+    #         else:
+    #             d_pay[k] = 1
+    #         amt -= k
+    #         pocket[k] -= 1
+    # if amt == 0:
+    #     return d_pay
+    # else:
+    #     for k, v in d_pay.items():
+    #         pocket[k] += v
+    #     return {}
 
 
 exec(input().strip())
